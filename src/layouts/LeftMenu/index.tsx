342aabfa5b124dfb137a485c6952a8a6572cf66b
ycl <499475142@qq.com>
2023-03-02 12:52:33 +0800

    搭建框架

--- src/layouts/LeftMenu/index.tsx
+++ src/layouts/LeftMenu/index.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import {Layout} from "antd";
+import React, {useState} from "react";
+import {Layout, MenuProps, Image} from "antd";
 import {inject, observer} from "mobx-react";
+import {Link} from "react-router-dom";
+import AdminConfig from "@/config/global";
+import favicon from "@/assets/images/favicon.png";
+
 const {Sider} = Layout;
 
 const LeftMenu: React.FC = ({global}: any) => {
+  // 定义 menu 类型
+  type MenuItem = Required<MenuProps>["items"][number];
+  const [menuList, setMenuList] = useState<MenuItem[]>([]);
+
   return (
     <Sider
       trigger={null}
@@ -19,7 +27,23 @@ const LeftMenu: React.FC = ({global}: any) => {
     >
       <div className="layout-sider">
         <div className="pro-sider scroll">
-          菜单部分
+          <div className="menu">
+            <Link to={AdminConfig.HOME_ROUTER_PATH}>
+              <div className="hd-64 mgr-01 dis-fl ai-ct jc-ct">
+                <Image width={25} src={favicon} preview={false}/>
+                {global.isCollapse ?
+                  '' : <p style={{
+                    fontWeight: 'bold',
+                    margin: '0 12px',
+                    fontSize: '16px',
+                    color: 'white'
+                  }}>
+                    {AdminConfig.ADMIN_NAME}
+                  </p>
+                }
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
 
